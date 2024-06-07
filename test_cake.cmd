7833ec79fbc6f6d65ce96dc9012554f85739760c
sys_jenkins_swfish <sys_jenkins_swfish@intel.com>
2024-06-07 14:08:44 +0530

    t1

--- test_cake.cmd
+++ test_cake.cmd
@@ -1,11 +1,8 @@
-@ECHO OFF
-SETLOCAL
-SET target=%1
-IF "%target%"=="" (SET target=Default)
-SET env=%2
-IF "%env%"=="" (SET env=Development)
+ECHO Starting the ps script
 
-ECHO Running build target %target% for environment %env%
-ECHO Hello, World
+
+powershell -File ./deployment.ps1 
+
+ECHO Completed the PS script
 
 EXIT /B
